c8e158db132b64b1bc3afcbab9b4a1627aaa32d7
Rahul <rahulpatil989018@gmail.com>
2021-11-29 19:05:08 +0530

    spinner design

---
+++ src/components/Layout/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Spinner.css';
+export default function Spinner() {
+  return (
+    // <div>
+      <div className="wrapper">
+        <div className="circle circle2"></div>
+    </div>
+  )
+}
