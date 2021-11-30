ed297ffefdbd813d85e00682459467900ff08b94
Rahul <rahulpatil989018@gmail.com>
2021-11-30 19:39:04 +0530

    all tracks display and track card  with info

--- src/components/Tracks/Tracks.js
+++ src/components/Tracks/Tracks.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Consumer } from '../../context'
 
+import TrackCard from './TrackCard';
 import Spinner from '../Layout/Spinner';
 
 export default class Tracks extends Component{
@@ -14,7 +15,18 @@ export default class Tracks extends Component{
         return <Spinner/>
       }
       else{
-        return <h1>Feched successfully</h1>
+        // return <h1>Feched successfully</h1>
+        return (
+        <React.Fragment>
+          <div className="row gy-3 ml-auto mt-5">
+            {track_list.map(item => (
+            <TrackCard key={item.track.track_id} track={item.track}/>
+            )
+            )}
+          </div>
+        </React.Fragment>
+        )
+
       }
     }
   }
